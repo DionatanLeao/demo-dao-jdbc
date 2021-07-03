8463ff58f53f5818cf4d793e9b361b33cd38f1ce
DionatanLeao <dionatan.ti@outlook.com>
2021-07-03 09:47:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 /***
  * 
@@ -14,8 +17,9 @@ public class Program {
 
 		Departament obj = new Departament(1, "Books");
 
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Bob", "bob@email.com", new Date(), 3000.00, obj);
 
+		System.out.println(seller);
 	}
 
 }
