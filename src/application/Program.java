af7238e9d776533c19367878ed6138fed16835fd
DionatanLeao <dionatan.ti@outlook.com>
2021-07-03 09:22:32 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.entities.Departament;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departament obj = new Departament(1, "Books");
+
+		System.out.println(obj);
+
+	}
+
+}
