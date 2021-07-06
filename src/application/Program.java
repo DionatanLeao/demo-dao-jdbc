bb623c548892852409f37bbb9237d40f8d998499
DionatanLeao <dionatan.ti@outlook.com>
2021-07-06 19:22:56 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,10 +39,17 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("=== TEST 3: seller insert ===");
+		System.out.println("=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@email.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+
+		System.out.println("=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Maria");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+
 	}
 
 }
